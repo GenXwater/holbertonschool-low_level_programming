93b2f1a3853b3e73eb6cc780a783338ee4943590
GenXwater <louis.genty@outlook.com>
2024-06-17 16:14:50 +0200

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - use printf
+ *
+ * Return: the same
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+
+        return (0);
+}

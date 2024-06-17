40077d82280b24e06dd72c8837f5f03a16bc3ff7
GenXwater <louis.genty@outlook.com>
2024-06-17 16:36:58 +0200

    ok

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Printf
  *
- * Return: Always 0 (ok)
+ * Return: 0 (ok)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	return (0);
 }

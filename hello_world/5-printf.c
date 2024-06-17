40f697ef1abafa930a6ba9a15db3e50c7e3af757
GenXwater <louis.genty@outlook.com>
2024-06-17 16:25:21 +0200

    ok

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - use printf
+ *main - use printf
  *
- * Return: the same
+ *Return: the same
  */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }

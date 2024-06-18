30211c1da416ce9032a6707a32e57e4ceb1ffe4f
GenXwater <louis.genty@outlook.com>
2024-06-18 17:24:05 +0200

    ok

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,29 @@
+#include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 /**
  * main - Printf
  *
  * Return: 0 (ok)
  */
+
 int main(void)
 {
-        printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	int n;
+	int last_dgt;
 
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_dgt = n % 10;
+
+	if (n % 10 > 5)
+		printf("Last digit of %d and is greater than %d\n", n, n % 10);
+
+	else if (last_dgt == 0)
+		printf("Last digit of %d and is %d\n", n, last_dgt);
+
+	else
+		printf("Last digit of %d and is less than %d and not 0\n", n, n % 10);
+
+	return (0);
 }

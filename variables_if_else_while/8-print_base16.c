a73445deb7202a1f0cc4e9296329d797d36ec4c7
GenXwater <louis.genty@outlook.com>
2024-06-19 13:30:12 +0200

    display character base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,11 +9,15 @@
 
 int main(void)
 {
+	char (c);
 	int (n);
 
-	for (n = 0; n <= 15; n++)
-		printf("%d", n);
-	printf("\n");
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
 
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+			putchar('\n');
+	
 	return (0);
 }

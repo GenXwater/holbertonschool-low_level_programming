2b824ffbf5045c4b0b82dc18208952c257235754
GenXwater <louis.genty@outlook.com>
2024-06-19 09:02:56 +0200

    play with n % 10

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,13 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_dgt = n % 10;
 
 	if (n % 10 > 5)
 		printf("Last digit of %d and is greater than %d\n", n, n % 10);
 
-	else if (last_dgt == 0)
-		printf("Last digit of %d and is %d\n", n, last_dgt);
+	else if (n % 10 == 0)
+		printf("Last digit of %d and is %d\n", n, n % 10);
 
 	else
 		printf("Last digit of %d and is less than %d and not 0\n", n, n % 10);

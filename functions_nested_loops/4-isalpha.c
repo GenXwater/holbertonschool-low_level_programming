b8a00563cce9207df0e786cb356b2fc317ec16f3
GenXwater <louis.genty@outlook.com>
2024-06-21 14:11:30 +0200

    ok

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is lowercase
+ * @c: The character to check
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                return (1);
+        else
+                return (0);
+}       

cbeb935add5875ea16d9a6d4c0757aa757d6c7e1
GenXwater <louis.genty@outlook.com>
2024-06-21 15:40:16 +0200

    ok

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign - Cheks if n is greater than zero
- *
- * Return: 1 if n is graeter than 0, 0 if is zero and -1 if n is less than 0
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *        -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)

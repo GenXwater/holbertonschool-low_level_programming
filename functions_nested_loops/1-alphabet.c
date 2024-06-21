841ba401c77123a70bc066a6b6e18469387994eb
GenXwater <louis.genty@outlook.com>
2024-06-21 10:24:35 +0200

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - use new function
+ *
+ * Return: (0) Always
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c ='a'; c <= 'z'; c++)
+		_putchar(c);
+		_putchar('n');
+	return(0);
+}
---
+++ functions_nested_loops/one_exercise.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - use new function
+ *
+ * Return: (0) Always
+ */
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}

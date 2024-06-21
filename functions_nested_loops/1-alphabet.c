dd51d6a48231e8da7c4b54481ca471606ac2dd07
GenXwater <louis.genty@outlook.com>
2024-06-21 10:46:32 +0200

    bug fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Write a function that prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - Write a function that prints the alphabet, in lowercase, followed by a new linie
  */
-
 void print_alphabet(void)
 {
 	char c;

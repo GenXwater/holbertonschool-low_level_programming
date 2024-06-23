7c2c0f8cef0cd8788bb029bfc8a179b04b342dd1
GenXwater <louis.genty@outlook.com>
2024-06-23 22:39:49 +0200

    ok

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * times_table - affiche la table de multiplication par 9, en commençant par 0
+ * times_table - la table de multiplication par 9, en commençant par 0
+ *               Chaque produit est formaté avec une virgule et un espace
  */
 void times_table(void)
 {
@@ -9,37 +10,30 @@ void times_table(void)
 
 	for (ligne = 0; ligne <= 9; ligne++)
 	{
-
 		for (colonne = 0; colonne <= 9; colonne++)
 		{
 			produit = ligne * colonne;
 
-			if (colonne > 0)
+			if (colonne == 0)
+			{
+				_putchar('0' + produit);
+			}
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
 
-			if (produit < 10)
-			{
-				_putchar(' ');
+				if (produit <= 9)
+				{
+					_putchar(' ');
+					_putchar('0' + produit);
+				}
+				else
+				{
+					_putchar('0' + produit / 10);
+					_putchar('0' + produit % 10);
+				}
 			}
-			else
-			{
-				_putchar(' ');
-			}
-
-
-			if (produit >= 10)
-			{
-				_putchar((produit / 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-
-			_putchar((produit % 10) + '0');
 		}
 		_putchar('\n');
 	}

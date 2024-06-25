24b9ad75afb946ccb8d1215d6fe7583bf7b9738f
GenXwater <louis.genty@outlook.com>
2024-06-25 11:19:54 +0200

    print Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,8 +3,12 @@
 #include <time.h>
 
 /**
+ * main - Entry point
  *
+ * Description: Prints numbers from 0 to 100, replacing multiples of 3 with "Fizz"
+ *              and multiples of 5 with "Buzz".
  *
+ * Return: Always 0 (ok)
  */
 
 int main(void)
@@ -13,7 +17,11 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 && n != 0)
+		if (n % 3 == 0 && n != 0 && n % 5 == 0 && n != 0)
+		{
+			printf("Fizz ");
+		}		
+		else if (n % 3 == 0 && n != 0)
 		{
 			printf("Fizz ");
 		}

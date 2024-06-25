7243277deff643adbf18f73c609e290e18a06b16
GenXwater <louis.genty@outlook.com>
2024-06-25 11:28:02 +0200

    ok

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,14 +21,16 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}		
-		else if (n % 3 == 0 && n != 0)
+		else if (n % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (n % 5 == 0 && n != 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
 		}
+		else if (n == 100)
+			printf("Buzz");
 		else
 		{
 			printf("%d ", n);

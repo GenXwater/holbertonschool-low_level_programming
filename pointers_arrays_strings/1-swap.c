b51d2d15eff75ac9de467103c4366e5c20bec37b
GenXwater <louis.genty@outlook.com>
2024-06-26 13:31:20 +0200

    ok

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two intergers
+ *
+ * @*a : devient *b
+ * @*b : devient *a
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	a* = *b;
+	*b = temp;
+}

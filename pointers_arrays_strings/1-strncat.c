2659391f1998fe420fbfb4a4073993fe9950f636
GenXwater <louis.genty@outlook.com>
2024-06-30 19:11:56 +0200

    ok

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,36 +2,23 @@
 #include <stdio.h>
 
 /**
- * _strncat - concatène deux chaînes avec n caractères de src
- * @dest: chaîne de destination où sera ajoutée la chaîne source
- * @src: chaîne source à ajouter à la chaîne destination
- * @n: nombre maximum de caractères à ajouter de src à dest
+ * _strncat - Concatène deux chaînes avec n caractères de src
+ * @dest: Chaîne de destination où sera ajoutée la chaîne source
+ * @src: Chaîne source à ajouter à la chaîne destination
+ * @n: Nombre maximum de caractères à ajouter de src à dest
  *
- * Return: pointeur vers la chaîne de destination `dest`
+ * Return: Pointeur vers la chaîne de destination `dest`
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	/**
-	 * result - pointeur vers le début de la chaîne de destination
-	 */
 	char *result = dest;
-
-	/**
-	 * i - variable pour parcourir dest jusqu'à la fin
-	 */
 	int i = 0;
 
-	/**
-	 * Trouver la fin de dest
-	 */
 	while (*dest != '\0')
 	{
 		dest++;
 	}
 
-	/**
-	 * Ajouter jusqu'à n caractères de src à la fin de dest
-	 */
 	while (*src != '\0' && i < n)
 	{
 		*dest = *src;
@@ -40,16 +27,11 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 
-	/**
-	 * Si src a moins de n caractères, ajouter le caractère nul de fin
-	 */
 	if (i < n)
 	{
 		*dest = '\0';
 	}
 
-	/**
-	 * Retourner le pointeur vers la chaîne de destination originale
-	 */
-	return result;
+	return (result);
 }
+

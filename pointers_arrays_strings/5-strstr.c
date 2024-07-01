14f97e88bef9abe89ebfd88960e691cd0d9cd147
GenXwater <louis.genty@outlook.com>
2024-07-01 21:58:49 +0200

    ok

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * vérif chq. pos. de haystack pr trver la 1ere occurrence de needle
+ * 
+ * @needle: contient world!
+ * @haystack: contient hello world!
+ * Faire une bouble haystack, suivi en dedant d'un autre needle
+ * Faire comparer i + j à j, et si pas égale, alors break.
+ *
+ * Return 0 (ok)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+	}
+	return (0);
+}

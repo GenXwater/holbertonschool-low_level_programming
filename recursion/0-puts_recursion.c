f394f44470d22ce10a3471aee80389760b3c33a5
GenXwater <louis.genty@outlook.com>
2024-07-02 15:27:40 +0200

    ok

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts_recursion(char *s)
+{
+	int i;
+	
+	if (*s == '\0')
+	{
+		return;
+	}
+		
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		putchar(s[i]);
+	}
+	printf("\n");
+}
---
+++ recursion/README.md
@@ -0,0 +1,2 @@
+Ceci est mon readme
+N'hésitez pas à venir me voir, ici, on est bien

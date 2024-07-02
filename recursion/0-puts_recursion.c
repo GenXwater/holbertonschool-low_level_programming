b6a790f33151c05b1a2be23ecc44a9a885dbde7e
GenXwater <louis.genty@outlook.com>
2024-07-02 19:50:15 +0200

    ok

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: the string to print.
+ */
 void _puts_recursion(char *s)
 {
-	int i;
-	
 	if (*s == '\0')
 	{
+		putchar('\n');
 		return;
 	}
-		
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	printf("\n");
+	putchar(*s);
+	_puts_recursion(s + 1);
 }

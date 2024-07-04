02682800c95918d36fc0f93cce9c97a1a214ba94
GenXwater <louis.genty@outlook.com>
2024-07-04 15:41:45 +0200

    ok

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the program followed by a new line.
+ * main - Prints the name of the program followed by a new line
  * @argc: The number of command-line arguments (not used in this program).
  * @argv: An array of strings representing the command-line arguments.
  *
  * Return: 0 on success.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }

4d6c2737c43f440fa125d80bb41bfd65170700fd
GenXwater <louis.genty@outlook.com>
2024-07-04 15:53:15 +0200

    ok

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argc: The number of command-line arguments (not used in this program).
  * @argv: An array of strings representing the command-line arguments.
  *
- * Return: 0 on success.
+ * Return: 0 on succeiss
  */
 int main(int argc, char **argv)
 {

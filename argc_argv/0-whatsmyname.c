7622fa28a28e77d613a7f236829b27420a9474c0
GenXwater <louis.genty@outlook.com>
2024-07-04 15:20:51 +0200

    ok

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
  * main - Prints the name of the program followed by a new line.
  * @argc: The number of command-line arguments (not used in this program).
  * @argv: An array of strings representing the command-line arguments.
- * 
+ *
  * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
 

d2c0de3fd967682312327d8c6dc80bbf5b453357
GenXwater <louis.genty@outlook.com>
2024-07-04 16:55:59 +0200

    ok

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -5,7 +5,7 @@
  * main - Prints the name of the program followed by a new line
  * @argc: The number of command-line arguments (not used in this program).
  * @argv: An array of strings representing the command-line arguments.
- * 
+ *
  * Return: 0 on succeiss
  */
 int main(int argc, char **argv)
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 
 	for (i = 0; i < argc; i++)
 		{
-			printf("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
 		}
 	return (0);
 }

b3a98b3cc19729e307f987c054f95b8e1a266835
GenXwater <louis.genty@outlook.com>
2024-07-05 15:02:50 +0200

    ok

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Multiplies two numbers provided as command-line arguments.
@@ -9,28 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	int num1 = 0;
 	int num2 = 0;
-	int result;
+	int result = 0;
 
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
-	for (i = 0; argv[1][i] != '\0'; i++)
-	{
-		num1 = num1 * 10 + (argv[1][i] - '0');
-	}
-
-	for (i = 0; argv[2][i] != '\0'; i++)
-	{
-		num2 = num2 * 10 + (argv[2][i] - '0');
-	}
-
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);

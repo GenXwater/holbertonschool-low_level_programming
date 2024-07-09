44353ee553cb7777bca5d0a4bdb6efd5ef43088b
GenXwater <louis.genty@outlook.com>
2024-07-09 11:07:32 +0200

    ok

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * free_grid - Libère la mérmoire allouée pour un tableau 2D d'entiers
- * @grid: Le tableau 2D à libérer
- * @height: La hauteur du tableau
+ * free_grid - Libère la mémoire allouée pour un tableau 2D d'entiers.
+ * @grid: Le tableau 2D à libérer.
+ * @height: La hauteur du tableau.
  *
- * Description: Fonction qui libère la mémoire allouée pr 1 tbl 2D d'entiers
- * 		précédemment crée par la fct. alloc_grid. Elle libère la
- * 		mémoire allouée pr chq ligne du tableau, puis la mémoire
- * 		allouée pr les pointeurs d'netiers.
- * Return: Cette fonction ne retourn rien
+ * Description: Cette fonction libère la mémoire allouée pour un tableau 2D
+ * d'entiers précédemment créé par la fonction alloc_grid. Elle libère
+ * la mémoire allouée pour chaque ligne du tableau, puis la mémoire allouée
+ * pour les pointeurs d'entiers.
+ *
+ * Return: Cette fonction ne retourne rien.
  */
 void free_grid(int **grid, int height)
 {
@@ -24,7 +24,7 @@ void free_grid(int **grid, int height)
 	/** Libération de la mémoire allouée pr chq ligne du tableau */
 	for (i = 0; i < height; i++)
 		free(grid[i]); /** Libère la mémoire allouée pr une ligne */
-	
+
 	/** Libération de la mérmoire allouée pr les pointeurs d'entiers */
 	free(grid);
 }

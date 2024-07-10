4238b3a241963d6bbadc1e615f6bd24b369865f5
GenXwater <louis.genty@outlook.com>
2024-07-10 18:55:53 +0200

    ok

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
+    char *name; /**< Le nom du chien */
+    float age; /**< L'âge du chien */
+    char *owner; /**< Le nom du propriétaire du chien */
+};
+
+#endif /* DOG_H */
+

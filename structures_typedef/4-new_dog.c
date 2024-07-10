11a9b4569244b65fb7b454174af92090b3d8fbde
GenXwater <louis.genty@outlook.com>
2024-07-10 19:30:29 +0200

    ok

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,30 +8,25 @@
  * @age: L'âge du chien.
  * @owner: Le nom du propriétaire du chien.
  *
- * Return: Un pointeur vers la nouvelle structure allouée
+ * Return: Un pointeur vers la nouvelle structure allouée,
  * ou NULL en cas d'échec.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/** ptr > new structure allouée */
 	dog_t *new_dog_ptr;
+	int i;
 
-	/** Alloue de la mémoire pour la nouvelle structure */
 	new_dog_ptr = malloc(sizeof(dog_t));
-	if (new_dog_ptr == NULL) /** Si l'alloc échoue, renvoie NULL */
+	if (new_dog_ptr == NULL)
 		return (NULL);
 
-	/** Alloue mémoire pour chaînes de caract. name et owner */
 	new_dog_ptr->name = malloc(strlen(name) + 1);
-	 /** Si l'alloc échoue, libère la mém. allouée & renvoie NULL */
 	if (new_dog_ptr->name == NULL)
 	{
 		free(new_dog_ptr);
 		return (NULL);
 	}
 	new_dog_ptr->owner = malloc(strlen(owner) + 1);
-
-	/** Si l'alloc échoue, libère la mém allouée et renvoie NULL */
 	if (new_dog_ptr->owner == NULL)
 	{
 		free(new_dog_ptr->name);
@@ -39,11 +34,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	/** Copie les valeurs des paramètres dans la nouvelle structure */
-	strcpy(new_dog_ptr->name, name);
-	new_dog_ptr->age = age;
-	strcpy(new_dog_ptr->owner, owner);
+	for (i = 0; name[i] != '\0'; i++)
+		new_dog_ptr->name[i] = name[i];
+	new_dog_ptr->name[i] = '\0';
 
-	/** Retourne un pointeur vers la nouvelle structure allouée */
+	new_dog_ptr->age = age;
+
+	for (i = 0; owner[i] != '\0'; i++)
+		new_dog_ptr->owner[i] = owner[i];
+	new_dog_ptr->owner[i] = '\0';
+
 	return (new_dog_ptr);
 }
+

741debea070be62c2247c40facc3d4f0dcacd9be
GenXwater <louis.genty@outlook.com>
2024-07-10 18:58:19 +0200

    ok

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Définit une nouvelle structure pour représenter un chien.
+ * @name: Le nom du chien.
+ * @age: L'âge du chien.
+ * @owner: Le nom du propriétaire du chien.
+ */
 struct dog
 {
-    char *name; /**< Le nom du chien */
-    float age; /**< L'âge du chien */
-    char *owner; /**< Le nom du propriétaire du chien */
+	char *name; /**< Le nom du chien */
+	float age; /**< L'âge du chien */
+	char *owner; /**< Le nom du propriétaire du chien */
 };
 
 #endif /* DOG_H */

1c206b080cb8f81c73fb329af9b558e99797f44b
GenXwater <louis.genty@outlook.com>
2024-07-19 14:41:21 +0200

    ok

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,9 +16,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(op, n);
 
-	if (separator == NULL) {
-		separator = "(nil)";
-	}
+	if (separator == NULL)
+		separator = "";
 
 	for (i = 0; i < n; i++)
 	{

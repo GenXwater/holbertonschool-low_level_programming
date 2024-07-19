22692944add0a0d316b9e26f273b46d3b5fab1fc
GenXwater <louis.genty@outlook.com>
2024-07-19 13:34:32 +0200

    ok

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,18 +17,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(op, n);
 
 	if (separator == NULL)
-	{
 		separator = "";
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(op, int));
-		
+
 		if ( i < n - 1)
-		{
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
 

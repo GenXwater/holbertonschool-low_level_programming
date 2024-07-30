9bbd156497454dbb8b5eaadfcd4b8a7f914410a8
GenXwater <louis.genty@outlook.com>
2024-07-30 13:25:20 +0200

    fix Betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,21 +9,21 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-	    if (h->str != NULL)
-	    {
-		    printf("[%u] %s\n", h->len, h->str);
-	    }
-	    else
-	    {
-		    printf("[0] (nil)\n");
-	    }
-	    h = h->next;
-	    ++node_count;
-    }
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		++node_count;
+	}
 
-    return (node_count);
+	return (node_count);
 }

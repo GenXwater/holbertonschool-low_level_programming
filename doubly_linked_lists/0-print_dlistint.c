076484e1e3e5d95875235c0b64b874854db0cee2
GenXwater <louis.genty@outlook.com>
2024-07-31 15:58:33 +0200

    add and code file, wtested

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print the list
+ * @h: header
+ *
+ * Return: the numbre d'élements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+ok
+double linkedclists

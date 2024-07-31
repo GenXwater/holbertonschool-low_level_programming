7514315e99c773564b99ff502c093cc5998544ea
GenXwater <louis.genty@outlook.com>
2024-08-01 00:10:43 +0200

    correction for betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,9 +10,9 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
-	
+
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 	{
 		free(new_node);
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
-	
+
 	if (*head != NULL)
 	{
 		(*head)->prev = new_node;

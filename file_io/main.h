edb0ef93b8abc8a7f664cf1ac111fc885f82e1f9
GenXwater <louis.genty@outlook.com>
2024-08-03 00:14:23 +0200

    add code and push

---
+++ file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>  /** Pour size_t */
+#include <sys/types.h> /** Pour ssize_t */
+#include <fcntl.h>  /** pour open */
+#include <unistd.h> /** pour read, write et close */
+#include <stdlib.h> /** pour malloc et free */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
+

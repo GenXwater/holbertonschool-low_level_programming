0170014d9ad15998413f778fea76056e7dd7a81c
GenXwater <louis.genty@outlook.com>
2024-08-04 21:40:44 +0200

    ok

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index.
+ *
+ * @n: the number that must be printed after been converted in binary
+ * @index: index starting from 0 of the bit to set
+ * Return: returns 1 it succeeded or -1 if it failed
+ *
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bm;/**BitMask*/
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	bm = 1 << index;
+	*n = (*n | bm);
+
+	return (1);
+}
